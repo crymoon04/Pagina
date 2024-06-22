9659df574411d5b81d6b4f2290733c12a515d1fb
Axel Torres <104744161+AxlAleT@users.noreply.github.com>
2024-06-22 16:59:40 +0000

    prueba

--- tutorias/php/tutoresDisponibles.php
+++ tutorias/php/tutoresDisponibles.php
@@ -1,16 +1,18 @@
 <?php
+echo("0");
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "escom_registro_tutorias";
-
+echo("a");
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
-
+echo("b");
 $genero = $_POST['genero'];
+echo("c");
 
 $sql = "SELECT t.id, t.nombre, t.apellido_paterno, t.apellido_materno
         FROM tutores t
@@ -18,19 +20,25 @@ $sql = "SELECT t.id, t.nombre, t.apellido_paterno, t.apellido_materno
         WHERE t.genero = '$genero'
         GROUP BY t.id
         HAVING COUNT(et.id_estudiante) <= 15";
+echo("d");
 
 $result = $conn->query($sql);
+echo("e");
 
 $tutores = array();
+echo("f");
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $tutores[] = $row; 
     }
 }
+echo("g");
+
 
 header('Content-Type: application/json');
 echo json_encode($tutores);
+echo("f");
 
 $conn->close();
 ?>
